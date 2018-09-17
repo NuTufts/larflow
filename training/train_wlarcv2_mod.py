727dfc09b85a60fc91db7b284335cab9d5003efd
rsharankova <ralitsa.sharankova@gmail.com>
2018-09-17 11:23:06 -0400

    edit train script for u,v separate

--- training/train_wlarcv2_mod.py
+++ training/train_wlarcv2_mod.py
@@ -43,7 +43,7 @@ else:
     sys.path.append("../models")
                     
 # Our model definition
-from larflow_uresnet_mod2 import LArFlowUResNet
+from larflow_uresnet_mod import LArFlowUResNet
 #from larflow_uresnet import LArFlowUResNet
 from larflow_loss import LArFlowLoss
 
@@ -69,9 +69,9 @@ ADC_THRESH=10.0
 VISI_WEIGHT=0.001
 USE_VISI=False
 #DEVICE_IDS=[3,4,5]
-DEVICE_IDS=[1]
-GPUID2=DEVICE_IDS[0]
-GPUID1=1
+DEVICE_IDS=[0,1]
+GPUID1=DEVICE_IDS[0]
+GPUID2=DEVICE_IDS[1]
 # map multi-training weights 
 #CHECKPOINT_MAP_LOCATIONS={"cuda:2":"cuda:0",
 #                          "cuda:3":"cuda:1",
@@ -80,7 +80,7 @@ CHECKPOINT_MAP_LOCATIONS={"cuda:0":"cuda:2",
                           "cuda:1":"cuda:3",
                           "cuda:2":"cuda:4"}
 CHECKPOINT_MAP_LOCATIONS=None
-CHECKPOINT_FROM_DATA_PARALLEL=True
+CHECKPOINT_FROM_DATA_PARALLEL=False
 # ===================================================
 
 
@@ -102,7 +102,7 @@ def main():
 
     # create model, mark it to run on the GPU
     if GPUMODE:
-        model = LArFlowUResNet(inplanes=16,input_channels=1,num_classes=2,showsizes=False, use_visi=USE_VISI, gpuid1=GPUID1, gpuid2=GPUID2) #False
+        model = LArFlowUResNet(inplanes=32,input_channels=1,num_classes=2,showsizes=False, use_visi=USE_VISI, gpuid1=GPUID1, gpuid2=GPUID2)
         #model.cuda(GPUID) # put onto gpuid
     else:
         model = LArFlowUResNet(inplanes=16,input_channels=1,num_classes=2)
@@ -134,16 +134,16 @@ def main():
         criterion = LArFlowLoss(maxdist,VISI_WEIGHT)
 
     # training parameters
-    lr = 1.0e-5
+    lr = 1.0e-4
     momentum = 0.9
     weight_decay = 1.0e-4
 
     # training length
-    batchsize_train = 4*len(DEVICE_IDS)
-    batchsize_valid = 2*len(DEVICE_IDS)
+    batchsize_train = 2#*len(DEVICE_IDS)
+    batchsize_valid = 1#*len(DEVICE_IDS)
     start_epoch = 0
     epochs      = 10
-    num_iters   = 10000 #30000
+    num_iters   = 30000
     iter_per_epoch = None # determined later
     iter_per_valid = 10
     iter_per_checkpoint = 300
