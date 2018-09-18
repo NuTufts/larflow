315b4bb443423942e6b05e97c4c19f0e06097e05
twongjirad <taritree.wongjirad@gmail.com>
2018-09-18 00:24:57 -0400

    fixed larcv2 intersection data

--- gen3dconsistdata/gendata_larcv2.cxx
+++ gen3dconsistdata/gendata_larcv2.cxx
@@ -8,15 +8,19 @@
 #include "TTree.h"
 
 // NOTE: LARCV2
-#incude "larcv/DataFormat/IOManager.h"
-#incude "larcv/DataFormat/EventImage2D.h"
+#include "larcv/core/DataFormat/IOManager.h"
+#include "larcv/core/DataFormat/EventImage2D.h"
+#include "larcv/core/DataFormat/Image2D.h"
+#include "larcv/core/DataFormat/ImageMeta.h"
 
 int main(int nargs, char** argv ) {
 
   const larutil::Geometry* geo = larutil::Geometry::GetME();
 
   larcv::IOManager io(larcv::IOManager::kWRITE );
-  io.add_in_file("consistency3d_data.root");
+  io.set_out_file("consistency3d_data.root");
+  io.initialize();
+  
   larcv::ImageMeta y2u_meta( 0, 0, 3456, 2400, 2400, 3456, 0 );
   larcv::ImageMeta y2v_meta( 0, 0, 3456, 2400, 2400, 3456, 1 );  
   larcv::Image2D y2u_intersect_y( y2u_meta );
@@ -31,11 +35,11 @@ int main(int nargs, char** argv ) {
   std::vector<larcv::Image2D> y2v_intersect_v;
   y2v_intersect_v.emplace_back( std::move(y2v_intersect_y) );
   y2v_intersect_v.emplace_back( std::move(y2v_intersect_z) );
-  
+
+  std::vector<int> nwires_v = { 2400, 2400, 3456 };
   double ypos;
   double zpos;
   for (int y=0; y<3456; y++) {
-    source_wire = y;
     for (int puv=0; puv<2; puv++) {
       for (int wire=0; wire<nwires_v[puv]; wire++) {
   	geo->IntersectionPoint( y, wire, 2, puv, ypos, zpos );
@@ -52,6 +56,7 @@ int main(int nargs, char** argv ) {
   }
 
   // save to image2d
+  io.set_id(0,0,0);  
   auto ev_y2u = (larcv::EventImage2D*)io.get_data("image2d", "y2u_intersect");
   ev_y2u->emplace( std::move(y2u_intersect_v) );
   auto ev_y2v = (larcv::EventImage2D*)io.get_data("image2d", "y2v_intersect");
