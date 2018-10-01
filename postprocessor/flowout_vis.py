c01fbc5f457676a0551339a229d1de4ae782b48e
Ralitsa <ralitsa.sharankova@gmail.com>
2018-10-01 17:40:44 -0400

    dWall from mctruth

--- postprocessor/flowout_vis.py
+++ postprocessor/flowout_vis.py
@@ -10,7 +10,7 @@ argparser = argparse.ArgumentParser(description="pyqtgraph visualization for DL 
 argparser.add_argument("-i", "--input",    required=True,  type=str, help="location of input larlite file with larflow3dhit tree")
 argparser.add_argument("-mc","--mctruth",  default=None,   type=str, help="location of input larlite file with mctrack and mcshower objects")
 argparser.add_argument("-e", "--entry",    required=True,  type=int, help="entry number")
-argparser.add_argument("-c", "--color",    required=True,  type=str, help="colorscheme. options: [ssnet,quality,flowdir,infill,3ddist,hastruth]")
+argparser.add_argument("-c", "--color",    required=True,  type=str, help="colorscheme. options: [ssnet,quality,flowdir,infill,3ddist,hastruth,dwall]")
 args = argparser.parse_args(sys.argv[1:])
 
 # Setup pyqtgraph/nump
@@ -49,8 +49,8 @@ io.add_in_filename( inputfile )
 io.open()
 
 # color scheme
-schemes = ["colorbyssnet","colorbyquality","colorbyflowdir","colorbyinfill","colorby3ddist","colorbyhastruth"]
-shortschemes = ["ssnet","quality","flowdir","infill","3ddist","hastruth"]
+schemes = ["colorbyssnet","colorbyquality","colorbyflowdir","colorbyinfill","colorby3ddist","colorbyhastruth","colorbydwall"]
+shortschemes = ["ssnet","quality","flowdir","infill","3ddist","hastruth","dwall"]
 
 colorscheme = args.color
 if colorscheme in shortschemes:
@@ -133,10 +133,24 @@ for ihit in xrange(nhits):
             colors[ihit,:] = (1.,0.,1.,1.)
 
     elif colorscheme=="colorbyhastruth":
-        if hit.trackid!=-1:
+        if hit.truthflag==1:
             colors[ihit,:] = (1.,1.,1.,1.) # mctrack matched
+        elif hit.truthflag==2:
+            colors[ihit,:] = (1.,0.,0.,1.) # mctrack tails matched
+        elif hit.truthflag==0:
+            colors[ihit,:] = (0.,1.,0.,1.) # mctrack not matched
         else:
-            colors[ihit,:] = (1.,0.,0.,1.) # mctrack not matched
+            colors[ihit,:] = (1.,1.,1.,1.) # kUnknown
+
+    elif colorscheme=="colorbydwall":
+        if hit.dWall>15.:
+            colors[ihit,:] = (1.,1.,1.,1.)
+        elif hit.dWall==-1:
+            colors[ihit,:] = (0.,1.,0.,1.)
+        else:
+            colors[ihit,:] = (1.,0.,0.,1.)
+        if hit.endpt_score>0.8:
+            colors[ihit,:] = (0.,0.,1.,1.)
 
 hitplot = gl.GLScatterPlotItem(pos=pos_np, color=colors, size=2.0, pxMode=False)
 
