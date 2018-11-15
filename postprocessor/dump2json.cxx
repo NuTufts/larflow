e6b02aa504e358343928a74dd4e2eebf6d608c35
twongjirad <taritree.wongjirad@gmail.com>
2018-11-15 18:15:08 -0500

    add utility to dump output to json file

---
+++ postprocessor/dump2json.cxx
@@ -0,0 +1,213 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+#include <TApplication.h>
+
+// larlite
+#include "DataFormat/storage_manager.h"
+#include "DataFormat/larflow3dhit.h"
+#include "DataFormat/larflowcluster.h"
+#include "DataFormat/mcshower.h"
+#include "DataFormat/mctrack.h"
+
+// cluster
+#include "cluster/TruthCluster.h"
+
+#include "nlohman_json/json.hpp"
+
+using json = nlohmann::json;
+
+struct InputArgs_t {
+  
+  InputArgs_t()
+    : hasinput(false),
+      outputset(false)
+  {};
+
+  bool hasinput;
+  bool hasmc;
+  bool outputset;
+  std::string input_larflowhits;
+  std::string input_mcinfo;  
+  std::string output_json;
+};
+
+struct Arg_t {
+  Arg_t( std::string f, std::string h ) 
+    : flag(f),
+      help(h)
+  {};
+  std::string flag;
+  std::string help;
+};
+
+InputArgs_t parseArgs( int nargs, char** argv ) {
+
+  std::vector< Arg_t > commands;
+  commands.push_back( Arg_t("-i", "input: larflow hits [required]") );
+  commands.push_back( Arg_t("-mc","input: mcinfo [required]") );
+  commands.push_back( Arg_t("-o", "json file name [required]") );  
+
+  bool printhelp = false;
+  for ( int iarg=1; iarg<nargs; iarg++ )
+    if ( std::string(argv[iarg])=="-h" ) printhelp = true;
+  
+  if ( nargs==1 || printhelp ) {
+    std::cout << "-------------------------------" << std::endl;
+    std::cout << "Dev arguments" << std::endl;
+    for ( auto const& arg : commands ) {
+      std::cout << "  " << arg.flag << "  :: " << arg.help << std::endl;
+    }
+    throw std::runtime_error("end of help");
+  }
+
+  InputArgs_t argconfig;
+  
+  for ( int iarg=1; iarg<nargs; iarg++ ) {
+
+    if ( argv[iarg][0]!='-' )
+      continue;
+
+    bool commandmatched = false;
+    for ( auto const& command : commands ) {
+      
+      if ( std::string(argv[iarg])==command.flag ) {
+	commandmatched = true;
+	std::string strflag = argv[iarg];
+	
+	if ( command.flag=="-i" ) {
+	  argconfig.input_larflowhits = argv[iarg+1];
+	  argconfig.hasinput = true;
+	}
+	else if ( command.flag=="-mc" ) {
+	  argconfig.input_mcinfo = argv[iarg+1];
+	  argconfig.hasmc = true;
+	}
+	else if ( command.flag=="-o" ) {
+	  argconfig.output_json = argv[iarg+1];
+	  argconfig.outputset = true;
+	}
+	// operation complete
+	break;
+      }//end of if command flag matched
+    }
+
+    if ( !commandmatched ) {
+      std::stringstream ss;
+      ss << "unrecognized command: '" << argv[iarg]<< "'";
+      throw std::runtime_error(ss.str());
+    }
+
+  }//end of loop over arguments
+
+  // Configuration Check
+  // -------------------
+  if ( !argconfig.hasinput || !argconfig.outputset || !argconfig.hasmc ) {
+    throw std::runtime_error("missing input and/or output files");
+  }
+  
+  return argconfig;
+}
+
+
+int main( int nargs, char** argv ) {
+
+  std::cout << "conversion from larflow hit larlite to json file" << std::endl;
+
+  // expects cropped output (from deploy/run_larflow_wholeview.py)
+  // -------------------------------------------------------------
+
+  // arg parsing
+  InputArgs_t inputargs = parseArgs( nargs, argv );
+  
+  TApplication app ("app",&nargs,argv);  
+
+  std::cout << "===========================================" << std::endl;
+  std::cout << " JSON conversion tool " << std::endl;
+  std::cout << " -------------------------- " << std::endl;
+  
+  // hit (and mctruth) event data
+  larlite::storage_manager io( larlite::storage_manager::kREAD );
+  io.add_in_filename( inputargs.input_larflowhits );
+  io.open();
+    
+  int nentries = io.get_entries();
+  std::cout << "Number of entries in file: " << nentries << std::endl;
+
+  larflow::TruthCluster clusteralgo;
+  
+  json jall; // empty structure
+  
+  for (int ientry=0; ientry<nentries; ientry++) {
+
+    io.go_to(ientry);
+
+    auto ev_lfhits   = (larlite::event_larflow3dhit*)io.get_data( larlite::data::kLArFlow3DHit, "flowhits" );
+    auto ev_mctrack  = (larlite::event_mctrack*) io.get_data( larlite::data::kMCTrack,  "mcreco" );
+    auto ev_mcshower = (larlite::event_mcshower*)io.get_data( larlite::data::kMCShower, "mcreco" );
+    
+    std::vector< std::vector<const larlite::larflow3dhit*> > cluster_v = clusteralgo.clusterHits( *ev_lfhits, *ev_mctrack, *ev_mcshower, true, true );
+    
+    json jevent; // event object
+    std::vector<float> x_v(ev_lfhits->size(),0);
+    std::vector<float> y_v(ev_lfhits->size(),0);
+    std::vector<float> z_v(ev_lfhits->size(),0);
+    std::vector<int>   c_v(ev_lfhits->size(),-1);
+
+    int ihit=0;
+    int icluster=0;
+    for ( auto const& hit_v : cluster_v ) {
+      
+      int cid=-1;
+      if ( icluster+1<cluster_v.size() )
+	cid = icluster;
+      
+      for ( auto const& phit : hit_v ) {
+	float x=(*phit)[0];
+	float y=(*phit)[1];
+	float z=(*phit)[2];
+
+	if ( x==-1 && x==y && x==z )
+	  continue;
+
+	x_v[ihit] = x;
+	y_v[ihit] = y;
+	z_v[ihit] = z;
+	c_v[ihit] = cid;
+
+	ihit++;
+      }
+      
+      icluster++;
+    }
+
+    x_v.resize(ihit);
+    y_v.resize(ihit);
+    z_v.resize(ihit);
+    c_v.resize(ihit);
+
+    json jx(x_v);
+    json jy(y_v);
+    json jz(z_v);
+    json jc(c_v);
+
+    jevent["x"] = jx;
+    jevent["y"] = jy;
+    jevent["z"] = jz;
+    jevent["c"] = jc;
+
+    char zevent[200];
+    sprintf(zevent,"event%d",ientry);
+    jall[zevent] = jevent;
+    
+  }//end of entry loop
+
+  std::ofstream out(inputargs.output_json.c_str());
+  out << std::setw(4) << jall << std::endl;
+  out.close();
+  
+  return 0;
+
+}
