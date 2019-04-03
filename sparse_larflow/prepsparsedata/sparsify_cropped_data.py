a156d682080edb8a3b52ce7d8825c55bb78a5b0a
twongjirad <taritree.wongjirad@gmail.com>
2019-04-03 13:07:40 -0400

    save first draft of cropper

--- sparse_larflow/prepsparsedata/sparsify_cropped_data.py
+++ sparse_larflow/prepsparsedata/sparsify_cropped_data.py
@@ -6,7 +6,7 @@ from ublarcvapp import ublarcvapp
 from ROOT import std
 
 def gen_default_split_cfg(wire_producer):
-    split_cfg="""Verbosity: 2
+    split_cfg="""Verbosity: 0
     InputProducer: "%s"
     OutputBBox2DProducer: "detsplit"
     CropInModule: true
@@ -16,10 +16,10 @@ def gen_default_split_cfg(wire_producer):
     CoveredZWidth: 310
     FillCroppedYImageCompletely: true
     DebugImage: false
-    MaxImages: -1
-    RandomizeCrops: false
+    MaxImages: 3
+    RandomizeCrops: true
     MaxRandomAttempts: 50
-    MinFracPixelsInCrop: 0.01
+    MinFracPixelsInCrop: 0.0001
     """%(wire_producer)
     return split_cfg
 
@@ -50,46 +50,130 @@ def gen_default_larflowcrop_cfg():
     """
     return lfcrop_cfg
 
+def default_processor_config(input_adc_producer,input_chstatus_producer):
+    processor_cfg="""ProcessDriver: {
+    Verbosity: 0
+    EnableFilter: false
+    RandomAccess: false
+    ProcessType: ["UBSplitDetector","UBCropLArFlow"]
+    ProcessName: ["Split","Crop"]
+
+    IOManager: {
+      Verbosity: 2
+      Name: "IOManager"
+      IOMode: 2
+      OutFileName: "test_crop_larflow.root"
+      InputFiles: []
+      InputDirs: []
+      StoreOnlyType: []
+      StoreOnlyName: []
+      TickBackward: true
+    }
+
+    ProcessList: {
+      Split: {
+        Verbosity: 0
+        InputProducer: "%s"
+        OutputBBox2DProducer: "detsplit"
+        CropInModule: true
+        OutputCroppedProducer: "detsplit"
+        BBoxPixelHeight: 512
+        BBoxPixelWidth: 832
+        CoveredZWidth: 310
+        FillCroppedYImageCompletely: true
+        DebugImage: false
+        MaxImages: 20
+        RandomizeCrops: true
+        MaxRandomAttempts: 50
+        MinFracPixelsInCrop: 0.0001
+      }
+      Crop: {
+        Verbosity:0
+        InputADCProducer: "%s"
+        InputBBoxProducer: "detsplit"
+        InputCroppedADCProducer: "detsplit"
+        InputVisiProducer: ""
+        InputFlowProducer: "%s"
+        InputChStatusProducer: "%s"
+        OutputCroppedADCProducer: "croppedadc"
+        OutputCroppedVisiProducer: "croppedvisi"
+        OutputCroppedFlowProducer: "croppedflow"
+        OutputCroppedMetaProducer: "croppedmeta_defunct"
+        IsMC: true
+        HasVisibilityImage: false
+        UseVectorizedCode: true # always true now
+        MaxImages: 10
+        Thresholds: [10.0,10.0,10.0]
+        DoMaxPool: false # not working
+        RowDownsampleFactor: 1 # not working
+        ColDownsampleFactor: 1 # not working
+        RequireMinGoodPixels: true # not working
+        LimitOverlap: true
+        MaxOverlapFraction: 0.25
+        CheckFlow: false # not working
+        MakeCheckImage: false # not working
+        SaveTrainingOutput: true
+        OutputFilename: "croppedout.root"
+      }
+    }    
+}"""%(input_adc_producer,input_adc_producer,"larflow",input_chstatus_producer)
+    return processor_cfg
+    
+
 def sparsify_crops(inputfile, outputfile,
                    adc_producer="wiremc", flow_producer="larflow",
+                   chstatus_producer="wiremc",
                    split_config=None,
                    flowdirs=['y2u','y2v']):
 
     nflows = len(flowdirs)
-    
-    io = larcv.IOManager(larcv.IOManager.kREAD,"",larcv.IOManager.kTickBackward)
+
+    # create a processor
+
+    # first create cfg file
+    processor_cfg = default_processor_config(adc_producer,"wiremc")
+    print(processor_cfg,file=open("cropflow_processor.cfg",'w'))
+    processor = larcv.ProcessDriver( "ProcessDriver" )
+    processor.configure( "cropflow_processor.cfg" )
+    io = processor.io()
     io.add_in_file(inputfile)
-    io.specify_data_read(larcv.kProductImage2D,adc_producer)
-    io.specify_data_read(larcv.kProductImage2D,flow_producer)
-    io.initialize()
+    processor.initialize()
 
-    split_cfg  = gen_default_split_cfg(adc_producer)
-    lfcrop_cfg = gen_default_larflowcrop_cfg()
-
-    split_algo = ublarcvapp.UBSplitDetector()
-    print(split_cfg,file=open("ubsplit.cfg",'w'))
-    split_pset = larcv.CreatePSetFromFile( "ubsplit.cfg", "UBSplitDetector" )    
-    split_algo.configure(split_pset)
-    split_algo.initialize()
+    nentries = processor.io().get_n_entries()
     
     out = larcv.IOManager(larcv.IOManager.kWRITE,"")
     out.set_out_file(outputfile)
     out.initialize()
 
-    for ientry in xrange(io.get_n_entries()):
-        io.read_entry(ientry)
+    for ientry in xrange(nentries):
 
-        ev_adc  = io.get_data(larcv.kProductImage2D,"wiremc")
-        ev_flow = io.get_data(larcv.kProductImage2D,"larflow")
-        adc_v  = ev_adc.Image2DArray()
-        flow_v = ev_flow.Image2DArray()
+        print("=======================")
+        print("process entry[%d]"%(ientry))
+        print("=======================")
+        
+        processor.process_entry(ientry, False, False) # we set autosave to off. else it will clear the data products.
+        #processor.process_entry(ientry)
+        io = processor.io_mutable()
+        
 
+        #ev_adc  = io.get_data(larcv.kProductImage2D,"wiremc")
+        #ev_flow = io.get_data(larcv.kProductImage2D,"larflow")
+        #adc_v  = ev_adc.Image2DArray()
+        #flow_v = ev_flow.Image2DArray()
+
+
+        ev_crops = io.get_data(larcv.kProductImage2D,"detsplit")
+        ev_rois  = io.get_data(larcv.kProductROI,"detsplit")
         # random splits
-        crops_v = std.vector("larcv::Image2D")()
-        rois_v  = std.vector("larcv::ROI")()
-        split_algo.process(adc_v, crops_v, rois_v)
-        print("number of random crops: %d"%(rois_v.size()))
+        #crops_v = std.vector("larcv::Image2D")()
+        #rois_v  = std.vector("larcv::ROI")()
+        #split_algo.process(adc_v, crops_v, rois_v)
+        print("number of random cropped images: %d"%(ev_crops.Image2DArray().size()))
+        print("number of random cropped ROIs:   %d"%(ev_rois).ROIArray().size())
 
+        ev_outflo = io.get_data(larcv.kProductImage2D,"croppedflow")
+        print("number of cropped flow images: %d"%(ev_outflo.Image2DArray().size()))
+        
         # processok = split_algo.process( adc_v, crops_v, rois_v )
 
         # # for larflow, we pack up to 5 images together, depending on flows
@@ -168,7 +252,7 @@ def sparsify_crops(inputfile, outputfile,
         break
 
     out.finalize()
-    io.finalize()
+    processor.finalize()
 
 if __name__ == "__main__":
     """
