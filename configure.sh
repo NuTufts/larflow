8f3a20cba2a41b38349239457c388844d987527f
twongjirad <taritree.wongjirad@gmail.com>
2019-04-10 10:40:55 -0400

    update config for ubdl use

--- configure.sh
+++ configure.sh
@@ -10,38 +10,8 @@ export OPENCV_LIBDIR=/usr/local/lib
 export OPENCV_INCDIR=/usr/local/include
 export USE_OPENCV=1
 
-git submodule update
 
-# setup larlite environment variables
-cd larlite
-source config/setup.sh
-
-# setup geo2d tool library
-cd ../Geo2D
-source config/setup.sh
-
-# setup LArOpenCV algorithms
-cd ../LArOpenCV
-source setup_laropencv.sh
-
-# setup larlite environment variabls
-cd ../larcv
-source configure.sh
-
-# setup larlitecv
-cd ../larlitecv
-source configure.sh
-
-# add larcvdataset folder to pythonpath
-cd ../larcvdataset
-source setenv.sh
-
-# add larcvdataset folder to pythonpath
-cd ../serverfeed
-source setenv.sh
-
-# return to top-level directory
-cd ../
+#git submodule update
 
 # setup post-processor
 export LARFLOW_POST_LIBDIR=${LARFLOW_BASEDIR}/postprocessor/lib
