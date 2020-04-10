c408c7a4331900aab13c68897e266f7a6b2f90b2
twongjirad <taritree.wongjirad@gmail.com>
2020-04-10 02:13:09 -0400

    setup data loading

--- larmatchnet/load_larmatch_kps.py
+++ larmatchnet/load_larmatch_kps.py
@@ -13,8 +13,8 @@ Load LArMatch Triplet data files for training and deploy
 """
 
 
-def load_larmatch_kps(tchain, current_entry,
-                           npairs=50000,verbose=False):
+def load_larmatch_kps(loader, current_entry,
+                      npairs=50000,verbose=False):
     
     data    = {"entry":current_entry}
 
@@ -22,35 +22,20 @@ def load_larmatch_kps(tchain, current_entry,
     
     # get data from match tree
     tio     = time.time()
-    nbytes  = tchain.GetEntry(current_entry)
+    nbytes  = loader.load_entry(current_entry)
     nfilled = c_int()
     nfilled.value = 0
-    spdata_v    = [ tchain.triplet_v[0].make_sparse_image( p ) for p in xrange(3) ]
-    index_array = tchain.triplet_v[0].sample_triplet_matches( npairs, nfilled, True )    
+    spdata_v    = [ loader.ttriplet.triplet_v[0].make_sparse_image( p ) for p in xrange(3) ]
+    matchdata   = loader.sample_data( npairs, nfilled, True )
+    data.update(matchdata)
     dtio        = time.time()-tio
-    print "index_array: ",index_array.shape
-
-    ssnet_label  = np.zeros((index_array.shape[0]),  dtype=np.long)
-    ssnet_weight = np.zeros((index_array.shape[0]),  dtype=np.float)
-    kpd_label    = np.zeros((index_array.shape[0]),  dtype=np.long)
-    kpd_shift    = np.zeros((index_array.shape[0],3),dtype=np.float)
-    for i in xrange(index_array.shape[0]):
-        ssnet_label[i]  = tchain.trackshower_label_v[index_array[i,3]]
-        ssnet_weight[i] = tchain.trackshower_weight_v[index_array[i,3]]
-        kpd_label[i]    = tchain.kplabel[index_array[i,3]][0]
-        for j in xrange(3):
-            kpd_shift[i,j] = tchain.kplabel[index_array[i,3]][1+j]
 
     # prepare data dictionary
     for p in xrange(3):
         data["coord_%d"%(p)] = spdata_v[p][:,0:2].astype( dtype=np.int32 )
         data["feat_%d"%(p)]  = spdata_v[p][:,2].reshape( (spdata_v[p].shape[0], 1) )
-    data["matchpairs"] = index_array[:,0:3].astype( dtype=np.long )
-    data["larmatchlabels"]  = index_array[:,3].astype( dtype=np.long )
-    data["ssnetlabels"]     = ssnet_label
-    data["ssnetweights"]    = ssnet_weight
-    data["kplabels"]        = kpd_label
-    data["kpshifts"]        = kpd_shift
+    data["matchpairs"]     = matchdata["matchtriplet"][:,0:3].astype( dtype=np.long )
+    data["larmatchlabels"] = matchdata["matchtriplet"][:,3].astype( dtype=np.long )
     data["npairs"]     = nfilled.value
 
     tottime = time.time()-t_start
@@ -84,13 +69,11 @@ if __name__ == "__main__":
     
     for ientry in xrange(nentries):
         print "[LOAD ENTRY ",ientry,"]"
-        loader.load_entry( ientry )
-        data = loader.sample_data( 50000, nfilled, True )
-        print type(data)
+        data = load_larmatch_kps( loader, ientry, verbose=True )
         for x,arr in data.items():
             if isinstance(arr,np.ndarray):
                 print x,": ",arr.shape
-                print "   ",arr
+                #print "   ",arr
             else:
                 print x,": ",arr
 
