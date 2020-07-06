239642333354727da2ee4bc49e2d4d65a976c277
polabr <polabr@umich.edu>
2020-07-06 15:53:05 -0400

    Extended 1 MLP to 3 MLPs for the different categories

--- larmatchnet/larmatch_keypoint_classifier.py
+++ larmatchnet/larmatch_keypoint_classifier.py
@@ -11,16 +11,38 @@ class LArMatchKeypointClassifier(nn.Module):
                  device=torch.device("cpu")):
         super(LArMatchKeypointClassifier,self).__init__()
 
-        # CLASSIFER: CLOSE-TO-KEYPOINT
-        keypoint_layers = OrderedDict()
-        keypoint_layers["keypoint0conv"] = torch.nn.Conv1d(ninput_planes*features_per_layer,
+        # CLASSIFER: CLOSE-TO-KEYPOINT TRACK ENDS
+        keypoint_trkEnds_layers = OrderedDict()
+        keypoint_trkEnds_layers["keypoint0conv"] = torch.nn.Conv1d(ninput_planes*features_per_layer,
                                                            keypoint_nfeatures[0],1)
-        keypoint_layers["keypoint0relu"] = torch.nn.ReLU()
+        keypoint_trkEnds_layers["keypoint0relu"] = torch.nn.ReLU()
         for ilayer,nfeats in enumerate(keypoint_nfeatures[1:]):
-            keypoint_layers["keypoint%dconv"%(ilayer+1)] = torch.nn.Conv1d(nfeats,nfeats,1)
-            keypoint_layers["keypoint%drelu"%(ilayer+1)] = torch.nn.ReLU()
-        keypoint_layers["keypointout"] = torch.nn.Conv1d(nfeats,1,1)
-        self.keypoint = torch.nn.Sequential( keypoint_layers )
+            keypoint_trkEnds_layers["keypoint%dconv"%(ilayer+1)] = torch.nn.Conv1d(nfeats,nfeats,1)
+            keypoint_trkEnds_layers["keypoint%drelu"%(ilayer+1)] = torch.nn.ReLU()
+        keypoint_trkEnds_layers["keypointout"] = torch.nn.Conv1d(nfeats,1,1)
+        self.keypoint_trkEnds = torch.nn.Sequential( keypoint_trkEnds_layers )
+
+        # CLASSIFER: CLOSE-TO-KEYPOINT SHOWER START
+        keypoint_shwrStart_layers = OrderedDict()
+        keypoint_shwrStart_layers["keypoint0conv"] = torch.nn.Conv1d(ninput_planes*features_per_layer,
+                                                           keypoint_nfeatures[0],1)
+        keypoint_shwrStart_layers["keypoint0relu"] = torch.nn.ReLU()
+        for ilayer,nfeats in enumerate(keypoint_nfeatures[1:]):
+            keypoint_shwrStart_layers["keypoint%dconv"%(ilayer+1)] = torch.nn.Conv1d(nfeats,nfeats,1)
+            keypoint_shwrStart_layers["keypoint%drelu"%(ilayer+1)] = torch.nn.ReLU()
+        keypoint_shwrStart_layers["keypointout"] = torch.nn.Conv1d(nfeats,1,1)
+        self.keypoint_shwrStart = torch.nn.Sequential( keypoint_shwrStart_layers )
+
+        # CLASSIFER: CLOSE-TO-KEYPOINT NEUTRINO VERTEX
+        keypoint_nuVtx_layers = OrderedDict()
+        keypoint_nuVtx_layers["keypoint0conv"] = torch.nn.Conv1d(ninput_planes*features_per_layer,
+                                                           keypoint_nfeatures[0],1)
+        keypoint_nuVtx_layers["keypoint0relu"] = torch.nn.ReLU()
+        for ilayer,nfeats in enumerate(keypoint_nfeatures[1:]):
+            keypoint_nuVtx_layers["keypoint%dconv"%(ilayer+1)] = torch.nn.Conv1d(nfeats,nfeats,1)
+            keypoint_nuVtx_layers["keypoint%drelu"%(ilayer+1)] = torch.nn.ReLU()
+        keypoint_nuVtx_layers["keypointout"] = torch.nn.Conv1d(nfeats,1,1)
+        self.keypoint_nuVtx = torch.nn.Sequential( keypoint_nuVtx_layers )
         
     def forward(self,triplet_feat_t):
         """
@@ -30,6 +52,17 @@ class LArMatchKeypointClassifier(nn.Module):
         inputs:
         triplet_feat_t : tensor where each row is concatenated feature vector        
         """
-        pred = self.keypoint(triplet_feat_t)
+        pred_trkEnds = self.keypoint_trkEnds(triplet_feat_t)
+        pred_shwrStart = self.keypoint_shwrStart(triplet_feat_t)
+        pred_nuVtx = self.keypoint_nuVtx(triplet_feat_t)
+        pred = torch.cat((pred_trkEnds, pred_shwrStart, pred_nuVtx), 1)
         return pred
     
+
+if __name__=="__main__":
+    print "test"
+    a = LArMatchKeypointClassifier()
+    b = torch.zeros(1,48,100)
+    print b.shape
+    c = a.forward(b)
+    print c.shape
