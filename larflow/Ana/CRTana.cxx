c6ff10e55d834dfb77ef9411069be04aac964d42
polabr <polabr@umich.edu>
2020-08-31 16:01:06 -0400

    Array of hitsPerVoxel sizes output [DEBUG]

--- larflow/Ana/CRTana.cxx
+++ larflow/Ana/CRTana.cxx
@@ -42,8 +42,14 @@ int main( int nargs, char** argv ) {
   float yyxMax[] = {117., 117., 256.};
   int yyxBins[] = {234, 234, 256};
 
+  int voxelSize[] = {1, 3, 5, 10}; // cm
+  
   // Input for ttree
-  int hitsPerVoxel;
+  //int hitsPer1cmVoxel;
+  //int hitsPer3cmVoxel;
+  //int hitsPer5cmVoxel;
+  //int hitsPer10cmVoxel;
+  int hitsPerVoxel[4];
   
   std::string input_crtfile = argv[1];
   int startentry = atoi(argv[2]);
@@ -57,25 +63,36 @@ int main( int nargs, char** argv ) {
   
   TFile* outfile = new TFile(Form("crt_%d-%d.root",startentry,startentry+maxentries-1),"recreate");
   TTree *tree = new TTree("tree","tree of hits per voxel");
-  tree->Branch("hitsPerVoxel", &hitsPerVoxel, "hitsPerVoxel/I");
- 
+  //  tree->Branch("hitsPer1cmVoxel", &hitsPer1cmVoxel, "hitsPer1cmVoxel/I");
+  //tree->Branch("hitsPer3cmVoxel", &hitsPer3cmVoxel, "hitsPer3cmVoxel/I");
+  //tree->Branch("hitsPer5cmVoxel", &hitsPer5cmVoxel, "hitsPer5cmVoxel/I");
+  //tree->Branch("hitsPer10cmVoxel", &hitsPer10cmVoxel, "hitsPer10cmVoxel/I");
+  tree->Branch("hitsPerVoxel_array", hitsPerVoxel, "hitsPerVoxel[4]/I");
+  //  tree->Branch("hitsPerVoxel[0]", &hitsPerVoxel[0], "hitsPerVoxel_0/I");
+  //  tree->Branch("hitsPerVoxel[1]", &hitsPerVoxel[1], "hitsPerVoxel_1/I");
+  //tree->Branch("hitsPerVoxel[2]", &hitsPerVoxel[2], "hitsPerVoxel_2/I");
+  //tree->Branch("hitsPerVoxel[3]", &hitsPerVoxel[3], "hitsPerVoxel_3/I");
+  
   // Define histograms
   const int nhists = 3;
   std::string str1[3] = {"U","V","Y"};
   std::string str2[3] = {"x","y","z"};
   std::string str3[3] = {"x","z","z"};
   std::string str4[3] = {"y","y","x"};
+
+  const int voxHists = 4;
+  std::string str5[4] = {"1cm","3cm","5cm","10cm"};
   
   // wire hists
   TH1D* hitcount_wire_hist[ nhists ] = {nullptr};
-  for (int n = 0; n < 3; n++ ) {
+  for (int n = 0; n < nhists; n++ ) {
     char name[100];
     sprintf( name, "hitcount_wire_hist_%s", str1[n].c_str() );
     hitcount_wire_hist[n] = new TH1D( name, "wire #", wireBins[n], 0, wireMax[n]);
   }
 
   TH2D* hitcount_wire_th2d[ nhists ] = {nullptr};
-  for (int n = 0; n < 3; n++ ) {
+  for (int n = 0; n < nhists; n++ ) {
     char name[100];
     sprintf( name, "hitcount_wire_th2d_%s", str1[n].c_str() );
     hitcount_wire_th2d[n] = new TH2D( name, "wire #; tick", wireBins[n]*10, 0, wireMax[n], 1008, 2400, 8448);
@@ -83,23 +100,25 @@ int main( int nargs, char** argv ) {
 
   // xyz hists
   TH1D* hitcount_xyz_hist[ nhists ] = {nullptr};
-  for (int n = 0; n < 3; n++ ) {
+  for (int n = 0; n < nhists; n++ ) {
     char name[100];
     sprintf( name, "hitcount_xyz_hist_%s", str2[n].c_str() );
     hitcount_xyz_hist[n] = new TH1D( name, ";position", xyzBins[n], xyzMin[n], xyzMax[n]);
   }
 
   TH2D* hitcount_xyz_th2d[ nhists ] = {nullptr};
-  for (int n = 0; n < 3; n++ ) {
+  for (int n = 0; n < nhists; n++ ) {
     char name[100];
     sprintf( name, "hitcount_xyz_th2d_%s%s", str3[n].c_str(), str4[n].c_str() );
     hitcount_xyz_th2d[n] = new TH2D( name, ";position ; position", xzzBins[n], xzzMin[n], xzzMax[n], yyxBins[n], yyxMin[n], yyxMax[n]);
   }
 
-  TH3D* hitcount_xyz_th3d = nullptr;
-  char name[100];
-  sprintf( name, "hitcount_xyz_th3d");
-  hitcount_xyz_th3d = new TH3D( name, ";position ;position ; position", (256/5), 0., 256., (234/5), -117., 117., (1036/5), 0., 1036.);
+  TH3D* hitcount_xyz_th3d[ voxHists ] = {nullptr};
+  for (int n = 0; n < voxHists; n++ ) {
+    char name[100];
+    sprintf( name, "hitcount_xyz_th3d_%s", str5[n].c_str() );
+    hitcount_xyz_th3d[n] = new TH3D( name, ";position ;position ; position", (xyzBins[0]/voxelSize[n]), xyzMin[0], xyzMax[0], (xyzBins[1]/voxelSize[n]), xyzMin[1], xyzMax[1], (xyzBins[2]/voxelSize[n]), xyzMin[2], xyzMax[2]);
+  }
 
   // Loop over events
   for (int i = startentry; i < (startentry + maxentries); i++) {
@@ -133,26 +152,33 @@ int main( int nargs, char** argv ) {
 	hit_y = lfhit[1];
 	hit_z = lfhit[2];
 
-	// fill wire hists
+	// fill wire 1d hists
 	hitcount_wire_hist[0]->Fill(hit_U);
 	hitcount_wire_hist[1]->Fill(hit_V);
 	hitcount_wire_hist[2]->Fill(hit_Y);
 
+	// fill wire 2d hists
 	hitcount_wire_th2d[0]->Fill(hit_U, tick);
 	hitcount_wire_th2d[1]->Fill(hit_V, tick);
 	hitcount_wire_th2d[2]->Fill(hit_Y, tick);
 
-	// fill xyz hists
+	// fill xyz 1d hists
 	hitcount_xyz_hist[0]->Fill(hit_x);
 	hitcount_xyz_hist[1]->Fill(hit_y);
 	hitcount_xyz_hist[2]->Fill(hit_z);
 
+	// fill xyz 2d hists
 	hitcount_xyz_th2d[0]->Fill(hit_x, hit_y);
 	hitcount_xyz_th2d[1]->Fill(hit_z, hit_y);
 	hitcount_xyz_th2d[2]->Fill(hit_z, hit_x);
-
-	hitcount_xyz_th3d->Fill(hit_x, hit_y, hit_z);
-
+	
+	// fill 3d hists for 4 diff voxel sizes
+	//	for (int n = 0; n < 4; i++) hitcount_xyz_th3d[n]->Fill(hit_x, hit_y, hit_z);
+	hitcount_xyz_th3d[0]->Fill(hit_x, hit_y, hit_z);
+	hitcount_xyz_th3d[1]->Fill(hit_x, hit_y, hit_z);
+	hitcount_xyz_th3d[2]->Fill(hit_x, hit_y, hit_z);
+	hitcount_xyz_th3d[3]->Fill(hit_x, hit_y, hit_z);
+	
       }
 
 
@@ -161,16 +187,21 @@ int main( int nargs, char** argv ) {
   }
 
   // Outside event loop
-  for (int i = 1; i <= (xyzBins[0]/5); i++) { // here use i = 1, i <= max, NOT i = 0, i < max (bc bin 0 is underflow in ROOT histograms)
-    for (int j = 1; j <= (xyzBins[1]/5); j++) {
-      for (int k = 1; k <= (xyzBins[2]/5); k++) {
-	
-	//	std::cout << hitcount_xyz_th3d->GetBinContent(i, j, k) << std::endl;
-	hitsPerVoxel = hitcount_xyz_th3d->GetBinContent(i, j, k);
-	tree->Fill();
 
+  for (int n = 0; n < voxHists; n++) {
+    
+    for (int i = 1; i <= (xyzBins[0]/voxelSize[n]); i++) { // here use i = 1, i <= max, NOT i = 0, i < max (bc bin 0 is underflow in ROOT histograms)
+      for (int j = 1; j <= (xyzBins[1]/voxelSize[n]); j++) {
+	for (int k = 1; k <= (xyzBins[2]/voxelSize[n]); k++) {
+
+	  //	  hitsPer3cmVoxel = hitcount_xyz_th3d[1]->GetBinContent(i, j, k);
+	  hitsPerVoxel[n] = hitcount_xyz_th3d[n]->GetBinContent(i, j, k);
+	  tree->Fill();
+	  
+	}
       }
     }
+    
   }
   
   outfile->Write();
