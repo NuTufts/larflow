9cc1fbb75a2b0f57f758e9a8b75bfee5aa354f57
twongjirad <taritree.wongjirad@gmail.com>
2020-09-11 10:46:19 -0400

    add boilerplate for shower truth-reco matching analysis

---
+++ larflow/Reco/ShowerTruthRecoAna.cxx
@@ -0,0 +1,14 @@
+#include "ShowerTruthRecoAna.h"
+
+namespace larflow {
+namespace reco {
+
+  void ShowerTruthRecoAna::process( larcv::IOManager& iolcv,
+                                    larlite::storage_manager& ioll,
+                                    larflow::reco::NuVertexCandidate& nuvertex_v )
+
+  {
+  }
+  
+}
+}
