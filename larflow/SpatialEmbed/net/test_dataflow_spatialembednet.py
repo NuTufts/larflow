702bef494203b82beca2f36cfd518b8c075f3731
taritree <taritree.wongjirad@gmail.com>
2020-11-13 17:27:28 -0500

    switch order of root and net loading. destroy numpy arrays after torch conversion

--- larflow/SpatialEmbed/net/test_dataflow_spatialembednet.py
+++ larflow/SpatialEmbed/net/test_dataflow_spatialembednet.py
@@ -28,6 +28,17 @@ larcv.SetPyUtil()
 from spatialembednet import SpatialEmbedNet
 from loss_spatialembed import SpatialEmbedLoss
 
+device = torch.device("cpu")
+
+# LOAD NET and LOSS
+voxel_dims = (2048, 1024, 4096)
+net = SpatialEmbedNet(3, voxel_dims,
+                      input_nfeatures=3,
+                      nclasses=1,
+                      stem_nfeatures=16).to(device)
+
+criterion = SpatialEmbedLoss( dim_nvoxels=voxel_dims ).to(device)
+
 # LOAD TREES
 infile = rt.TFile(args.input_file)
 io = infile.Get("s3dembed")
@@ -37,13 +48,6 @@ print("NENTRIES: ",nentries)
 voxelloader = larflow.spatialembed.Prep3DSpatialEmbed()
 voxelloader.loadTreeBranches( io )
 
-# LOAD NET and LOSS
-voxel_dims = (2048, 1024, 4096)
-net = SpatialEmbedNet(3, voxel_dims,
-                      input_nfeatures=3,
-                      nclasses=1,
-                      stem_nfeatures=16)
-criterion = SpatialEmbedLoss( dim_nvoxels=voxel_dims )
     
 for ientry in range(nentries):
 
@@ -52,16 +56,18 @@ for ientry in range(nentries):
     print("voxel entries: ",data["coord_t"].shape)
 
     # convert into torch tensors
-    coord_t    = torch.from_numpy( data["coord_t"] )
-    feat_t     = torch.from_numpy( data["feat_t"] )
-    instance_t = torch.from_numpy( data["instance_t"] )
+    coord_t    = torch.from_numpy( data["coord_t"] ).to(device)
+    feat_t     = torch.from_numpy( data["feat_t"] ).to(device)
+    instance_t = torch.from_numpy( data["instance_t"] ).to(device)
     coord_t.requires_grad = False
     feat_t.requires_grad = False
     instance_t.requires_grad = False
     print "max(coord_t): ",(torch.max( coord_t[:,0]),torch.max(coord_t[:,1]),torch.max(coord_t[:,2]))
     print "coord_t nan inf: ",torch.isnan(coord_t).sum()," ",torch.isinf(coord_t).sum()
     print "feat_t nan inf: ",torch.isnan(feat_t).sum()," ",torch.isinf(feat_t).sum()
-    print "instance_t nan: ",torch.isnan(instance_t).sum()        
+    print "instance_t nan: ",torch.isnan(instance_t).sum()
+
+    del data
 
     # forward
     embed_t,seed_t = net( coord_t, feat_t, verbose=True )
