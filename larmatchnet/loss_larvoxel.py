6f527c8b84d75f9189ef5ce5c4e734587f100de5
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2021-09-14 10:26:46 -0400

    add code to begin real training of voxel net. tests show that train_dist_larvoxel converges more slowly that jam test script does -- though should be indentical implementations ...

---
+++ larmatchnet/loss_larvoxel.py
@@ -0,0 +1,260 @@
+from __future__ import print_function
+import os,sys
+import torch
+import torch.nn as nn
+from lovasz_losses import lovasz_softmax
+
+class LArVoxelLoss(nn.Module):
+    def __init__(self, eval_ssnet=False,
+                 eval_keypoint_label=True,
+                 eval_affinity_field=False):
+        super(LArVoxelLoss,self).__init__( )
+        self.eval_ssnet = eval_ssnet
+        self.eval_keypoint_label = eval_keypoint_label
+        self.eval_affinity_field = eval_affinity_field
+        self.larmatch_softmax = torch.nn.Softmax( dim=1 )
+        self.focal_loss_gamma = 2
+        self.larmatch_use_focal_loss = True
+        self.ssnet_use_lovasz_loss = False
+        
+    def forward(self, larmatch_pred, ssnet_pred, kplabel_pred, kpshift_pred, affinity_pred,
+                larmatch_label, ssnet_label, kp_label, kpshift_label, affinity_label,
+                larmatch_weight, ssnet_weight, kplabel_weight, affinity_weight,
+                verbose=False):
+
+        loss = self.larmatch_loss( larmatch_pred, larmatch_label, larmatch_weight, verbose )
+        flmloss = loss.detach().item()
+                
+        # SSNET
+        if self.eval_ssnet:
+            ssloss = self.ssnet_loss( ssnet_pred, ssnet_label, ssnet_weight, verbose )
+            loss += ssloss
+            fssloss = ssloss.detach().item()
+        else:
+            fssloss = 0.0
+
+        # KPLABEL
+        if self.eval_keypoint_label:
+            kploss = self.keypoint_loss( kplabel_pred, kp_label, kplabel_weight, verbose )
+            loss += kploss
+            fkploss = kploss.detach().item()
+        else:
+            fkploss = 0.0
+
+        # AFFINITY FIELD
+        if self.eval_affinity_field:
+            pafloss = self.affinity_field_loss( affinity_pred, affinity_label, affinity_weight, truematch_index, verbose )
+            loss += pafloss
+            fpafloss = pafloss.detach().item()
+        else:
+            fpafloss = 0.0
+        
+        return loss, flmloss, fssloss, fkploss, fpafloss
+
+    def larmatch_loss( self, larmatch_pred,
+                       larmatch_truth,
+                       larmatch_weight,
+                       verbose=False ):
+
+        # number of spacepoint goodness predictions to evaluate
+        npairs     = larmatch_pred.shape[0]
+
+        if verbose:
+            print("[SparseLArMatchKPSLoss::larmatch_loss]")
+            print("  pred num triplets: ",npairs)
+            
+        # convert int to float for subsequent calculations
+        fmatchlabel = larmatch_truth[:npairs].type(torch.float).requires_grad_(False)
+
+        if self.larmatch_use_focal_loss:
+            # p_t for focal loss
+            #print(larmatch_pred.shape, larmatch_pred)
+            p_t = torch.sigmoid( larmatch_pred.F )
+            p_t = fmatchlabel*(p_t[:,1]+1.0e-6) + (1-fmatchlabel)*(p_t[:,0]+1.0e-6) # p if y==1; 1-p if y==0
+            if larmatch_weight is not None:
+                loss = (-larmatch_weight[:npairs]*torch.log( p_t )*torch.pow( 1-p_t, self.focal_loss_gamma )).sum()
+            else:
+                loss = (-torch.log( p_t )*torch.pow( 1-p_t, self.focal_loss_gamma )).mean()
+        else:
+            # calculate loss using binary cross entropy
+            bce       = torch.nn.BCEWithLogitsLoss( reduction='none' )
+            loss      = (bce( larmatch_pred, fmatchlabel )*larmatch_weight[:npairs]).sum()
+            
+        if verbose:
+            lm_floss = loss.detach().item()            
+            print("  loss-larmatch: ",lm_floss)
+        return loss
+                       
+
+    def keypoint_loss( self, keypoint_score_pred,
+                       keypoint_score_truth,
+                       keypoint_weight,
+                       verbose=False):
+        sel_kplabel_pred = keypoint_score_pred
+        sel_kplabel      = keypoint_score_truth
+        if keypoint_weight is not None: sel_kpweight     = keypoint_weight
+        if verbose or True:
+            print("  keypoint_score_pred:  (sel) ",sel_kplabel_pred.shape," ",sel_kplabel_pred[:10])
+            print("  keypoint_score_truth: (orig) ",keypoint_score_truth.shape," (sel) ",sel_kplabel.shape," ",sel_kplabel[:10])
+            if keypoint_weight is not None: print("  keypoint_weight: (orig) ",keypoint_weight.shape," (sel)",sel_kpweight.shape," ",sel_kpweight[:10])
+            
+        fn_kp    = torch.nn.MSELoss( reduction='none' )
+        fnout = fn_kp( sel_kplabel_pred, sel_kplabel )
+        if verbose:
+            print("  fnout shape: ",fnout.shape)
+        if keypoint_weight is not None:
+            kp_loss  = (fnout*sel_kpweight).sum()
+        else:
+            kp_loss = fnout.mean()
+            
+        kp_floss = kp_loss.detach().item()
+        if verbose:
+            print(" loss-kplabel: ",kp_floss)
+
+        return kp_loss
+
+    def ssnet_loss( self, ssnet_pred,
+                    ssnet_truth,
+                    ssnet_weight,
+                    verbose=False):
+        npairs = ssnet_pred.shape[0]
+        nclasses = ssnet_pred.shape[1]
+        # only evalulate loss on pixels where true label
+        sel_ssnet_pred   = ssnet_pred
+        if verbose or True:
+            print("  sel_ssnet_pred: ",sel_ssnet_pred.shape)
+            print("  ssnet_truth: ",ssnet_truth.shape)
+            if ssnet_weight is not None: print("  ssnet_weight: ",ssnet_weight.shape)
+
+        fn_ssnet = torch.nn.CrossEntropyLoss( reduction='none' )
+        if ssnet_weight is not None:
+            ssnet_loss = (fn_ssnet( sel_ssnet_pred, ssnet_truth )*ssnet_weight).sum()/20.0
+        else:
+            ssnet_loss = (fn_ssnet( sel_ssnet_pred, ssnet_truth )).mean()
+            
+        if self.ssnet_use_lovasz_loss:
+            ssnet_pred_x  = torch.transpose( sel_ssnet_pred,1,0).reshape( (1,nclasses,npairs,1) )
+            ssnet_truth_y = ssnet_truth.reshape( (1,npairs,1) )
+            ssnet_loss += lovasz_softmax( ssnet_pred_x, ssnet_truth_y )
+            
+        if verbose:
+            ssnet_floss = ssnet_loss.detach().item()            
+            print(" loss-ssnet: ",ssnet_floss)
+
+        return ssnet_loss
+                    
+    
+
+    def affinity_field_loss( self, affinity_field_pred,
+                             affinity_field_truth,
+                             affinity_field_weight,
+                             truematch_index,
+                             verbose=False):
+        npairs = affinity_field_pred.shape[0]
+        if affinity_field_pred.shape[0]!=affinity_field_truth.shape[0]:
+            raise RuntimeError("dont trust this mode of calculation right now")            
+            sel_pred   = torch.index_select( affinity_field_pred, 0, truematch_index )
+            sel_weight = torch.index_select( affinity_field_weight, 0, truematch_index )
+            sel_truth  = torch.index_select( affinity_field_truth, 0, truematch_index )
+        else:
+            sel_pred   = affinity_field_pred
+            sel_weight = affinity_field_weight
+            sel_truth  = affinity_field_truth
+
+        if verbose:
+            print("  affinity pred: ",sel_pred.shape," ",sel_pred[:20,:])#,torch.sum(sel_pred*sel_pred,1)[:20]
+            print("  affinity truth: ",sel_truth.shape," ",torch.sum(sel_truth*sel_truth,1)[:20])
+            print("  affinity weight: ",sel_weight.shape,"  ",sel_weight[:20])
+
+        fn_mse = torch.nn.MSELoss( reduction='none' )
+        fn_out = torch.sum(fn_mse( sel_pred, sel_truth ),1)
+        if verbose:
+            print("  affinity fn: ",fn_out.shape)
+        af_loss = (fn_out*sel_weight).sum()
+        if verbose:
+            af_floss = af_loss.detach().item()
+            print(" loss-affinity-field: ",af_floss)
+        return af_loss
+
+
+if __name__ == "__main__":
+
+    # a quick unit test of the functions above
+
+    import ROOT
+    from ROOT import std
+    from larflow import larflow
+    from ctypes import c_int
+    import numpy as np
+    
+    # use the loader function for KPS data
+    from load_larmatch_kps import load_larmatch_kps
+
+    # test file
+    input_files = ["output_alldata.root"]
+    input_v = std.vector("string")()
+    for i in input_files:
+        input_v.push_back(i)
+
+    loaders = {"kps":larflow.keypoints.LoaderKeypointData( input_v ),
+               "affinity":larflow.keypoints.LoaderAffinityField( input_v )}
+    for name,loader in loaders.items():
+        loader.exclude_false_triplets( False )
+    nentries = loaders["kps"].GetEntries()
+    print("num entries: ",nentries)
+
+    device  = torch.device("cpu")    
+    nmax    = c_int()
+    nfilled = c_int()
+    nmax.value = 50000
+    batchsize = 1
+    
+    lossfn = SparseLArMatchKPSLoss( eval_ssnet=False,
+                                    eval_keypoint_label=True,
+                                    eval_keypoint_shift=False,
+                                    eval_affinity_field=True )
+
+    for ientry in xrange(0,nentries,batchsize):
+        print("[LOAD ENTRY ",ientry,"]")
+        data = load_larmatch_kps( loaders, ientry, batchsize,
+                                  npairs=10000,
+                                  exclude_neg_examples=False,
+                                  verbose=True,
+                                  single_batch_mode=True )
+        if ientry==0:
+            print("data contents:")
+            for name in data:
+                print("  ",name)
+
+        # we copy the truth to make the "predictions"
+        print("num positive examples: ",data["positive_indices"].shape[0])
+
+        # larmatch
+        larmatch_truth   = torch.from_numpy( data["larmatchlabels"] )
+        larmatch_predict = torch.from_numpy( np.copy( larmatch_truth ) ).type(torch.float)
+        lmloss = lossfn.larmatch_loss(  larmatch_predict,
+                                        larmatch_truth,
+                                        torch.from_numpy( data["match_weight"] ),
+                                        verbose=True )
+        
+        # keypoint
+        keypoint_truth   = torch.from_numpy( data["kplabel"] )
+        keypoint_predict = torch.from_numpy( np.copy( keypoint_truth ) )
+        kploss = lossfn.keypoint_loss(  keypoint_predict,
+                                        keypoint_truth,
+                                        torch.from_numpy( data["kplabel_weight"] ),
+                                        torch.from_numpy( data["positive_indices"]),
+                                        verbose=True )
+
+        # affinity
+        affinity_truth   = torch.from_numpy( data["paf_label"] )
+        affinity_predict = torch.from_numpy( np.copy( affinity_truth ) )
+        pafloss = lossfn.affinity_field_loss(  affinity_predict,
+                                               affinity_truth,
+                                               torch.from_numpy( data["paf_weight"] ),
+                                               torch.from_numpy( data["positive_indices"]),
+                                               verbose=True )
+
+        
+        break
+    
---
+++ larmatchnet/train_dist_larmatchvoxel.py
@@ -0,0 +1,252 @@
+#!/bin/env python
+
+"""
+TRAINING SCRIPT FOR LARMATCH+KEYPOINT+SSNET NETWORKS
+"""
+## IMPORT
+
+# python,numpy
+from __future__ import print_function
+import os,sys,argparse
+
+import shutil
+import time,datetime
+import traceback
+import numpy as np
+
+# torch
+import torch.multiprocessing as mp
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.nn.functional as F
+import torch.distributed as dist
+
+# tensorboardX
+from torch.utils.tensorboard import SummaryWriter
+
+# larmatch imports
+import larvoxel_engine
+from larvoxel_dataset import larvoxelDataset
+from loss_larvoxel import LArVoxelLoss
+
+# ROOT, larcv
+import ROOT as rt
+from ROOT import std
+from larcv import larcv
+from larflow import larflow
+
+def cleanup():
+    dist.destroy_process_group()
+
+def run(gpu, args ):
+    #========================================================
+    # CREATE PROCESS
+    rank = args.nr * args.gpus + gpu
+    print("START run() PROCESS: rank=%d gpu=%d"%(rank,gpu))    
+    dist.init_process_group(                                   
+    	#backend='nccl',
+        backend='gloo',        
+   	#init_method='env://',
+        init_method='file:///tmp/sharedfile',
+    	world_size=args.world_size,                              
+    	rank=rank,
+        timeout=datetime.timedelta(0, 1800)
+    )
+    #========================================================
+    torch.manual_seed(gpu)
+
+    config = larvoxel_engine.load_config_file( args )
+
+    if rank==0:
+        tb_writer = SummaryWriter()
+
+    single_model, _ = larvoxel_engine.get_larmatch_model( config )
+
+    if config["RESUME_FROM_CHECKPOINT"]:
+        if not os.path.exists(config["CHECKPOINT_FILE"]):
+            raise ValueError("Could not find checkpoint to load: ",config["CHECKPOINT_FILE"])
+
+        checkpoint_data = larvoxel_engine.load_model_weights( single_model, config["CHECKPOINT_FILE"] )
+            
+    torch.cuda.set_device(gpu)
+    device = torch.device("cuda:%d"%(gpu) if torch.cuda.is_available() else "cpu")
+    single_model.to(device)
+
+    criterion = LArVoxelLoss( eval_ssnet=config["RUN_SSNET"],
+                              eval_keypoint_label=config["RUN_KPLABEL"] ).to(device)
+
+    # Wrap the model
+    model = nn.parallel.DistributedDataParallel(single_model, device_ids=[gpu],find_unused_parameters=False)
+    print("RANK-%d Model"%(rank),model)
+    torch.distributed.barrier()
+    
+    optimizer = torch.optim.Adam(model.parameters(),
+                                 lr=float(config["LEARNING_RATE"]), 
+                                 weight_decay=config["WEIGHT_DECAY"])
+    
+    if config["RESUME_FROM_CHECKPOINT"] and config["RESUME_OPTIM_FROM_CHECKPOINT"]:
+        optimizer.load_state_dict( checkpoint_data["optimizer"] )
+    
+    
+    # re-specify the dictionary
+    model_dict = {"larmatch":model}
+
+    train_dataset = larvoxelDataset( txtfile=config["INPUT_TXTFILE_TRAIN"], random_access=True )
+    if args.world_size>0:
+        train_dataset.set_partition( rank, args.world_size )
+    TRAIN_NENTRIES = len(train_dataset)
+    print("RANK-%d TRAIN DATASET NENTRIES: "%(rank),TRAIN_NENTRIES," = 1 epoch")
+    train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=1,collate_fn=larvoxelDataset.collate_fn)
+    sys.stdout.flush()
+
+    if rank==0:
+        valid_dataset = larvoxelDataset( txtfile=config["INPUT_TXTFILE_VALID"], random_access=True )
+        VALID_NENTRIES = len(valid_dataset)
+        print("RANK-%d: LOAD VALID DATASET NENTRIES: "%(rank),VALID_NENTRIES," = 1 epoch")
+        valid_loader = torch.utils.data.DataLoader(valid_dataset,batch_size=1,collate_fn=larvoxelDataset.collate_fn)
+    
+    loss_meters,acc_meters,time_meters = larvoxel_engine.make_meters(config)
+    
+    with torch.autograd.profiler.profile(enabled=config["RUNPROFILER"]) as prof:    
+        for iiter in range(config["NUM_ITERATIONS"]):
+            train_iteration = config["START_ITER"] + iiter
+            print("RANK-%d iteration=%d"%(rank,train_iteration))
+            larvoxel_engine.do_one_iteration(config,model_dict,train_loader,criterion,optimizer,
+                                             acc_meters,loss_meters,time_meters,True,device,verbose=None)
+
+            # periodic checkpoint
+            if iiter>0 and iiter%config["ITER_PER_CHECKPOINT"]==0:
+                if rank==0:
+                    print("RANK-0: saving periodic checkpoint")
+                    larvoxel_engine.save_checkpoint({
+                        'iter':train_iteration,
+                        'epoch': train_iteration/float(TRAIN_NENTRIES),
+                        'state_larmatch': model_dict["larmatch"].module.state_dict(),
+                        'optimizer' : optimizer.state_dict(),
+                    }, False, train_iteration)
+                else:
+                    print("RANK-%d: waiting for RANK-0 to save checkpoint"%(rank))                
+            
+            print("RANK-%d: current tree entry=%d"%(rank,train_dataset._current_entry))
+            if iiter%int(config["TRAIN_ITER_PER_RECORD"])==0 and rank==0:
+                # make averages and save to tensorboard, only if rank-0 process
+                larvoxel_engine.prep_status_message( "Train-Iteration", train_iteration, acc_meters, loss_meters, time_meters )
+
+                # write to tensorboard
+                # --------------------
+                # losses go into same plot
+                loss_scalars = { x:y.avg for x,y in loss_meters.items() }
+                tb_writer.add_scalars('data/train_loss', loss_scalars, train_iteration )
+                
+                # split acc into different types
+                # larmatch
+                acc_scalars = {}
+                for accname in larvoxel_engine.LM_CLASS_NAMES:
+                    acc_scalars[accname] = acc_meters[accname].avg
+                tb_writer.add_scalars('data/train_larmatch_accuracy', acc_scalars, train_iteration )
+
+                # ssnet
+                ssnet_scalars = {}
+                for accname in larvoxel_engine.SSNET_CLASS_NAMES+["ssnet-all"]:
+                    ssnet_scalars[accname] = acc_meters[accname].avg
+                tb_writer.add_scalars('data/train_ssnet_accuracy', ssnet_scalars, train_iteration )
+                
+                # keypoint
+                kp_scalars = {}
+                for accname in larvoxel_engine.KP_CLASS_NAMES:
+                    kp_scalars[accname] = acc_meters[accname].avg
+                tb_writer.add_scalars('data/train_kp_accuracy', kp_scalars, train_iteration )
+                
+                # paf
+                paf_acc_scalars = { "paf":acc_meters["paf"].avg  }
+                tb_writer.add_scalars("data/train_paf_accuracy", paf_acc_scalars, train_iteration )
+
+            if config["TRAIN_ITER_PER_VALIDPT"]>0 and iiter%int(config["TRAIN_ITER_PER_VALIDPT"])==0:
+                if rank==0:
+                    valid_loss_meters,valid_acc_meters,valid_time_meters = larvoxel_engine.make_meters(config)                    
+                    for viter in range(int(config["NUM_VALID_ITERS"])):
+                        with torch.no_grad():
+                            larvoxel_engine.do_one_iteration(config,{"larmatch":single_model},
+                                                             valid_loader,criterion,optimizer,
+                                                             valid_acc_meters,valid_loss_meters,valid_time_meters,
+                                                             False,device,verbose=False)
+                    larvoxel_engine.prep_status_message( "Valid-Iteration", train_iteration,
+                                                         valid_acc_meters,
+                                                         valid_loss_meters,
+                                                         valid_time_meters )
+                    # write to tensorboard
+                    # --------------------
+                    # losses go into same plot
+                    loss_scalars = { x:y.avg for x,y in loss_meters.items() }
+                    tb_writer.add_scalars('data/valid_loss', loss_scalars, train_iteration )
+                
+                    # split acc into different types
+                    # larmatch
+                    val_acc_scalars = {}
+                    for accname in larvoxel_engine.LM_CLASS_NAMES:
+                        val_acc_scalars[accname] = valid_acc_meters[accname].avg
+                    tb_writer.add_scalars('data/valid_larmatch_accuracy', val_acc_scalars, train_iteration )
+
+                    # ssnet
+                    val_ssnet_scalars = {}
+                    for accname in larvoxel_engine.SSNET_CLASS_NAMES+["ssnet-all"]:
+                        val_ssnet_scalars[accname] = valid_acc_meters[accname].avg
+                    tb_writer.add_scalars('data/valid_ssnet_accuracy', val_ssnet_scalars, train_iteration )
+                
+                    # keypoint
+                    val_kp_scalars = {}
+                    for accname in larvoxel_engine.KP_CLASS_NAMES:
+                        val_kp_scalars[accname] = valid_acc_meters[accname].avg
+                    tb_writer.add_scalars('data/valid_kp_accuracy', val_kp_scalars, train_iteration )
+                
+                    # paf
+                    val_paf_acc_scalars = { "paf":valid_acc_meters["paf"].avg  }
+                    tb_writer.add_scalars("data/valid_paf_accuracy", val_paf_acc_scalars, train_iteration )
+
+                else:
+                    print("RANK-%d process waiting for RANK-0 validation run"%(rank))
+
+                # wait for rank-0 to finish reporting and plotting
+                #torch.distributed.barrier()
+
+        print("RANK-%d process finished. Waiting to sync."%(rank))
+        if rank==0:
+            print("RANK-0: saving last checkpoint")
+            larvoxel_engine.save_checkpoint({
+                'iter':train_iteration,
+                'epoch': train_iteration/float(TRAIN_NENTRIES),
+                'state_larmatch': model_dict["larmatch"].module.state_dict(),
+                'optimizer' : optimizer.state_dict(),
+            }, False, train_iteration)
+        
+        torch.distributed.barrier()
+
+
+    cleanup()
+    return
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config-file', default="config_voxel.yaml", type=str,
+                        help='configuration file [default: config.yaml]')
+    parser.add_argument('-n', '--nodes', default=1, type=int, metavar='N',
+                        help='number of data loading workers (default: 4)')
+    parser.add_argument('-g', '--gpus', default=1, type=int,
+                        help='number of gpus per node')
+    parser.add_argument('-nr', '--nr', default=0, type=int,
+                        help='ranking within the nodes')
+    args = parser.parse_args()
+    args.world_size = args.gpus * args.nodes
+    
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '8888'
+    mp.spawn(run, nprocs=args.gpus, args=(args,), join=True)
+    
+    print("DISTRIBUTED MAIN DONE")
+    
+if __name__ == '__main__':
+
+    main()
