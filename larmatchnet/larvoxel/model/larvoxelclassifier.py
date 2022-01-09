cabf1ef98b29952449e301824ce78c1891e5bfc4
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2022-01-09 05:44:03 -0500

    more descriptions

--- larmatchnet/larvoxel/model/larvoxelclassifier.py
+++ larmatchnet/larvoxel/model/larvoxelclassifier.py
@@ -14,20 +14,31 @@ class LArVoxelClassifier(nn.Module):
                  embed_dim=512,
                  out_num_classes=6):
         """
+
+        Network consists of 
+          1) a ResNet encoder with a stem+6 layers. each layer downsamples.
+          2) a 3 layer convolution module that mixes features from each downsampled resnet layer
+          3) a global max and ave pooling layer that each merges the features at all pixels into one feature vector
+          3) an MLP that takes the concat of the pooling layers into the class predictions
+
         parameters
         -----------
         """
         super(LArVoxelClassifier,self).__init__()
+
+        # Number of dimensions of our input tensor
         self.D = 3
+
+        # Resnet encoder
         self.encoder = MinkEncode34C(in_channels=input_feats, out_channels=1, D=self.D)
 
-        print(self.encoder.PLANES)
-        
-        totchannels = self.encoder.INIT_DIM
+        # sum the channels at each output layer of the encoder
+        totchannels = self.encoder.INIT_DIM # the stem output channels
         for nch in self.encoder.PLANES:
-            totchannels += nch
-        print("tot channels: ",totchannels)
-        
+            totchannels += nch # output channels of each layer
+        #print("tot channels: ",totchannels)
+
+        # the convoutional layers that mixes features from all the resnet layers
         multiscale_layers = []
         multiscale_layers.append(
             nn.Sequential(
@@ -63,11 +74,14 @@ class LArVoxelClassifier(nn.Module):
                 ME.MinkowskiInstanceNorm(embed_dim),
                 ME.MinkowskiLeakyReLU(),
             ) )
-
         self.multiscale_conv = nn.Sequential(*multiscale_layers)
+
+        # the global pooling layers
+        # location within the dense tensor is lost
         self.global_max_pool = ME.MinkowskiGlobalMaxPooling()
         self.global_avg_pool = ME.MinkowskiGlobalAvgPooling()
 
+        # the final MLP
         self.final = nn.Sequential(
             self.get_mlp_block(embed_dim * 2, 512),
             ME.MinkowskiDropout(),
@@ -86,7 +100,7 @@ class LArVoxelClassifier(nn.Module):
         xsparse = xinput.sparse()
         encoder_out   = self.encoder(xsparse)
 
-        # we slice, which broadcasts pooled features back to original resolution?
+        # we slice, which broadcasts pooled features back to original resolution (I think)
         y = [ x.slice(xinput) for x in encoder_out ]
         y = ME.cat(y).sparse()
         
