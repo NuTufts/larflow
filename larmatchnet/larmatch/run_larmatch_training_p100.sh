7dd74c8439ef87fefe9343ffa97a162c893ac08a
Nick W. Dahlen <ndahle01@login-prod-02.pax.tufts.edu>
2024-09-26 11:36:22 -0400

    Committing localized changes

--- larmatchnet/larmatch/run_larmatch_training_p100.sh
+++ larmatchnet/larmatch/run_larmatch_training_p100.sh
@@ -4,7 +4,7 @@ slurmtag="slurm${SLURM_JOBID}"
 
 NGPUS=$1
 #CONFIG=/cluster/tufts/wongjiradlabnu/twongj01/gen2/ubdl/larflow/larmatchnet/larmatch/config/config_larmatchme_p100.yaml
-UBDL_DIR=/cluster/tufts/wongjiradlabnu/twongj01/gen2/photon_analysis/ubdl/
+UBDL_DIR=/cluster/tufts/wongjiradlabnu/ndahle01/ubdl/
 CONFIG=${UBDL_DIR}/larflow/larmatchnet/larmatch/config/config_larmatchme.yaml
 
 cd $UBDL_DIR
@@ -16,5 +16,6 @@ cd ${UBDL_DIR}/larflow/larmatchnet/larmatch
 
 rm -f /tmp/sharedfile
 echo "CONFIG: ${CONFIG}"
-python3 train_dist_larmatchme.py --config ${CONFIG} -n 1 --gpus 1 --no-parallel > /tmp/larmatch_training_out.${slurmtag}.log
-cp /tmp/larmatch_training_out.${slurmtag}.log ${UBDL_DIR}/larflow/larmatchnet/larmatch/
+
+python3 train_dist_larmatchme.py --config ${CONFIG} -n 1 --gpus 1 --no-parallel > /tmp/larmatch_training_out.log
+cp /tmp/larmatch_training_out_fhsjs.log ${UBDL_DIR}/larflow/larmatchnet/larmatch/
--- larmatchnet/larmatch/submit_larmatch_p1cmp075.sh
+++ larmatchnet/larmatch/submit_larmatch_p1cmp075.sh
@@ -9,8 +9,8 @@
 #SBATCH --partition=wongjiradlab
 #SBATCH --error=gridlog_train_larmatch.%j.%N.err
 
-# Change WORKDIR to be the folder where this script lives
-WORKDIR=/cluster/tufts/wongjiradlabnu/twongj01/gen2/photon_analysis/ubdl/larflow/larmatchnet/larmatch/
+WORKDIR=/cluster/tufts/wongjiradlabnu/ndahle01/ubdl/larflow/larmatchnet/larmatch
+
 container=/cluster/tufts/wongjiradlabnu/larbys/larbys-container/singularity_minkowskiengine_u20.04.cu111.torch1.9.0_comput8.sif
 module load singularity/3.5.3
 
