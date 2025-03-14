2b6cbc9bc1ed914c5df80bbd52938b4f54253e2d
Matthew Rosenberg <rosenberg6m@gmail.com>
2025-03-14 00:07:19 -0400

    updated larmatch and kpsrecoman python run scripts to fix implementation of default larcv image tickforward expectation with tickbackward flag for time reversal

--- larflow/Reco/test/run_kpsrecoman.py
+++ larflow/Reco/test/run_kpsrecoman.py
@@ -41,7 +41,10 @@ from larflow import larflow
 
 
 io = larlite.storage_manager( larlite.storage_manager.kBOTH )
-iolcv = larcv.IOManager( larcv.IOManager.kBOTH, "larcv", larcv.IOManager.kTickBackward )
+tickdir = larcv.IOManager.kTickForward
+if args.tickbackwards:
+    tickdir = larcv.IOManager.kTickBackward
+iolcv = larcv.IOManager( larcv.IOManager.kBOTH, "larcv", tickdir )
 
 print("[INPUT: DL MERGED] ",args.input_dlmerged)
 print("[INPUT: LARMATCH-KPS]  ",args.input_larflow)
@@ -123,7 +126,8 @@ iolcv.specify_data_read( "image2d", "ubspurn_plane1" )
 iolcv.specify_data_read( "image2d", "ubspurn_plane2" )
 iolcv.specify_data_read( "sparseimage", "sparseuresnetout" ) 
 #iolcv.addto_storeonly_list( ... )
-iolcv.reverse_all_products()
+if args.tickbackwards:
+    iolcv.reverse_all_products()
 
 io.set_out_filename( args.output.replace(".root","_larlite.root") )
 iolcv.set_out_file( args.output.replace(".root","_larcv.root") )
