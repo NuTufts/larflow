4566ec228c6192df1a2d5c1a02d8a4639ddb6915
Matthew Rosenberg <rosenberg6m@gmail.com>
2025-04-07 23:12:11 -0400

    include all prong pixels in pixel vector returned for LArPID and truth-matching so we can calcualate truth-matching variables properly; add flag to denote if pixel is in LArPID crop. This is a breaking change that requires the ntuple maker and any other script that runs LArPID to manually exclude pixels outside of the crop

--- larflow/PrepFlowMatchData/CropPixData_t.h
+++ larflow/PrepFlowMatchData/CropPixData_t.h
@@ -1,6 +1,8 @@
 #ifndef __LARFLOW_PREP_CROPPIXDATA_T_HH__
 #define __LARFLOW_PREP_CROPPIXDATA_T_HH__
 
+#include <cmath>
+
 namespace larflow {
   namespace prep {
     
@@ -15,7 +17,7 @@ namespace larflow {
     public:
       
     CropPixData_t()
-      : row(0),col(0),rawRow(0),rawCol(0),val(0.0),idx(0)
+      : row(0),col(0),rawRow(0),rawCol(0),val(0.0),inCrop(true),idx(0)
 	{};
       
       /** @brief constructor with row, col, value 
@@ -24,7 +26,7 @@ namespace larflow {
        *  @param[in] v value of pixel
        */      
     CropPixData_t( int r, int c, int rr, int rc, float v)
-      : row(r),col(c),rawRow(rr),rawCol(rc),val(v),idx(0) {};
+      : row(r),col(c),rawRow(rr),rawCol(rc),val(v),inCrop(true),idx(0) {};
 
       virtual ~CropPixData_t() {};
       
@@ -33,11 +35,12 @@ namespace larflow {
       int rawRow; ///< row of pixel in original image
       int rawCol; ///< col of pixel in original image
       float val; ///< value of pixel
+      bool inCrop; ///< pixel is inside crop
       int idx;   ///< index in container
 
       /** @brief equality operator based on row, col, and value */
       bool operator==( const CropPixData_t& rhs ) const {
-        if(rawRow == rhs.rawRow && rawCol == rhs.rawCol && val == rhs.val) return true;
+        if(rawRow == rhs.rawRow && rawCol == rhs.rawCol && fabs(val - rhs.val) < 1e-3) return true;
         return false;
       };
       
