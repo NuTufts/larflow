0c026051f48164d7c36e0239e2e1532034697ede
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2025-05-22 02:44:56 -0400

    made threshold for seeding prong lower. made threshold number of added hits to store re-interpretted track-to-shore cluster higher.

--- larflow/Reco/NuVertexShowerReco.cxx
+++ larflow/Reco/NuVertexShowerReco.cxx
@@ -773,12 +773,21 @@ namespace reco {
       }//loop over other prongs
 
       int num_hits_added = (int)shower_hit_v.size() - num_starting_hits;
-      LARCV_INFO() << "  number of hits added to shower: " << num_hits_added << " (before=" << shower_hit_v.size() << " after=" << num_starting_hits << ")" << std::endl;
+      float frac_added = 0.;
+      if ( num_starting_hits>0 )
+        frac_added = float(num_hits_added)/float(num_starting_hits);
+      LARCV_INFO() << "  hits added to shower: " 
+                   << num_hits_added << " (before=" << shower_hit_v.size() 
+                   << " after=" << num_starting_hits 
+                   << " frac=" << frac_added
+                   << ")" << std::endl;
+      
       if ( vtxcluster.type==larflow::reco::NuVertexCandidate::kTrack ) {
         // if we reinterpretted a track cluster as a shower prong, but it added no hits, 
         // we do not create a shower for it.
-        if ( num_hits_added<=0 ) {
-          LARCV_INFO() << "  re-interpretted track prong did not add hits. do not make shower." << std::endl;
+        
+        if ( frac_added<=0.30 ) {
+          LARCV_INFO() << "  re-interpretted track prong did not add enough hits. do not make shower." << std::endl;
           continue;
         }
 
